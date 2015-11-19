855ca9e0ad3a8b7defcd22352ac7c09d7de43c8a
Salifu Mutaru <salifumutaru@gmail.com>
2015-11-19 10:57:52 +0000

    Login working. Adapters for menu and order history working well.
    Now I need to replace string[] content in menu and order history by json
    from database. Also used local server, please change details

--- app/src/main/java/com/example/migaliza/chakula_user/orderHistory.java
+++ app/src/main/java/com/example/migaliza/chakula_user/orderHistory.java
@@ -1,21 +1,60 @@
 package com.example.migaliza.chakula_user;
 
-import android.app.Fragment;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+/**
+ * Created by Salifu on 11/18/2015.
+ */
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class orderHistory extends Fragment {
-    View rootview;
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p/>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class OrderHistory {
 
-    @Nullable
-    @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.fragment_order_history, container, false);
-        return rootview;
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<Order> ORDERS = new ArrayList<Order>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, Order> ORDER_MAP = new HashMap<>();
+
+    static {
+        // Add 3 sample items.
+        addItem(new Order("1", "banku hist"));
+        addItem(new Order("2", "Waakye ist"));
+        addItem(new Order("3", "Dorkuni hist"));
     }
 
+    private static void addItem(Order order) {
+        ORDERS.add(order);
+        ORDER_MAP.put(order.id, order);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class Order {
+        public String id;
+        public String content;
+
+        public Order(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
 }
+
